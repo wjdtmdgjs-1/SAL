300b630ddc7629a142fcb9997dfaa9d732eb0bcf
wjdtmdgjs-1 <jeong5207@naver.com>
2024-10-14 13:41:35 +0900

    init

---
+++ src/main/java/com/sparta/sal/SalApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.sal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/sal/SalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.sal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
