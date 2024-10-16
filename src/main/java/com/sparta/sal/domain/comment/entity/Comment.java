1a126a5de5e4c4a622692e34985a3c86e0f9231e
Juho <ljlh9509@gmail.com>
2024-10-17 04:35:24 +0900

    Fix : comment crud 권한 확인 추가

--- src/main/java/com/sparta/sal/domain/comment/entity/Comment.java
+++ src/main/java/com/sparta/sal/domain/comment/entity/Comment.java
@@ -1,11 +1,10 @@
 package com.sparta.sal.domain.comment.entity;
 
 import com.sparta.sal.common.entity.Timestamped;
-import com.sparta.sal.domain.card.dto.request.ModifyCardRequest;
 import com.sparta.sal.domain.card.entiry.Card;
 import com.sparta.sal.domain.comment.dto.request.ModifyCommentRequest;
 import com.sparta.sal.domain.comment.dto.request.SaveCommentRequest;
-import com.sparta.sal.domain.comment.dto.response.ModifyCommentResponse;
+import com.sparta.sal.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,6 +25,10 @@ public class Comment extends Timestamped {
     @JoinColumn(name = "card_id", nullable = false)
     private Card card;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
     public Comment(SaveCommentRequest reqDto){
         this.commentContent=reqDto.getContents();
         this.emoji=reqDto.getEmoji();
@@ -40,7 +43,11 @@ public class Comment extends Timestamped {
         this.isDeleted=true;
     }
 
-    public void setCard(Card card){
+    public void addCard(Card card){
         this.card=card;
+    }
+
+    public void addUser(User user) {
+        this.user = user;
     }
 }
--- src/main/java/com/sparta/sal/domain/comment/service/CommentService.java
+++ src/main/java/com/sparta/sal/domain/comment/service/CommentService.java
@@ -11,6 +11,11 @@ import com.sparta.sal.domain.comment.dto.response.ModifyCommentResponse;
 import com.sparta.sal.domain.comment.dto.response.SaveCommentResponse;
 import com.sparta.sal.domain.comment.entity.Comment;
 import com.sparta.sal.domain.comment.repository.CommentRepository;
+import com.sparta.sal.domain.member.entity.Member;
+import com.sparta.sal.domain.member.enums.MemberRole;
+import com.sparta.sal.domain.member.repository.MemberRepository;
+import com.sparta.sal.domain.user.entity.User;
+import com.sparta.sal.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,31 +28,47 @@ import java.util.List;
 public class CommentService {
     private final CommentRepository commentRepository;
     private final CardRepository cardRepository;
+    private final UserRepository userRepository;
+    private final MemberRepository memberRepository;
 
 
     @Transactional
-    public SaveCommentResponse saveComment(SaveCommentRequest reqDto, Long cardId) {
+    public SaveCommentResponse saveComment(AuthUser authUser, SaveCommentRequest reqDto, Long cardId) {
+        Card card = cardRepository.findById(cardId).orElseThrow(() -> new InvalidRequestException("카드를 찾을 수 없습니다."));
+        User user = userRepository.findById(authUser.getId()).orElseThrow(() -> new InvalidRequestException("사용자를 찾을 수 없습니다."));
+
+        Member member = checkRole(card, authUser);
+        if (member.getMemberRole().equals(MemberRole.READ_ONLY)) {
+            throw new InvalidRequestException("권한이 없습니다.");
+        }
+
         Comment comment = new Comment(reqDto);
-
-        Card card =cardRepository.findById(cardId).orElseThrow(() -> new InvalidRequestException("카드를 찾을 수 없습니다."));
-
-        comment.setCard(card);
+        comment.addCard(card);
+        comment.addUser(user);
 
         Comment savedComment = commentRepository.save(comment);
         return new SaveCommentResponse(savedComment);
     }
 
-    public List<GetCommentListResponse> getCommentList(Long cardId) {
+    public List<GetCommentListResponse> getCommentList(AuthUser authUser, Long cardId) {
+        Card card = cardRepository.findById(cardId).orElseThrow(() -> new InvalidRequestException("카드를 찾을 수 없습니다."));
+        checkRole(card, authUser);
+
         List<Comment> commentList = commentRepository.findByCard_IdAndIsDeletedOrderByCreatedAtAsc(cardId, false);
         return commentList.stream().map(GetCommentListResponse::new).toList();
     }
 
     @Transactional
-    public ModifyCommentResponse modifyComment(Long cardId, Long commentId, ModifyCommentRequest reqDto) {
+    public ModifyCommentResponse modifyComment(AuthUser authUser, Long cardId, Long commentId, ModifyCommentRequest reqDto) {
+        Card card = cardRepository.findById(cardId).orElseThrow(() -> new InvalidRequestException("카드를 찾을 수 없습니다."));
         Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new InvalidRequestException("댓글을 찾을 수 없습니다."));
 
-        if(comment.isDeleted()){
+        if (comment.isDeleted()) {
             throw new InvalidRequestException("삭제된 댓글입니다.");
+        }
+
+        if (!comment.getUser().getId().equals(authUser.getId())) {
+            throw new InvalidRequestException("작성자만 수정 또는 삭제 가능합니다");
         }
 
         comment.ModifyComment(reqDto);
@@ -56,9 +77,20 @@ public class CommentService {
 
     @Transactional
     public void deleteComment(AuthUser authUser, Long commentId) {
-        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new InvalidRequestException("card not found"));
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new InvalidRequestException("댓글을 찾을 수 없습니다."));
+        if (!comment.getUser().getId().equals(authUser.getId())) {
+            throw new InvalidRequestException("작성자만 수정 또는 삭제 가능합니다");
+        }
+
         comment.deleteComment();
         commentRepository.save(comment);
+    }
+
+    private Member checkRole(Card card, AuthUser authUser) {
+        Long workSpaceId = card.getList().getBoard().getWorkSpace().getId();
+        Member member = memberRepository.findByWorkSpace_IdAndUser_Id(workSpaceId, authUser.getId())
+                .orElseThrow(() -> new InvalidRequestException("해당 워크스페이스에 초대되지 않았습니다."));
+        return member;
     }
 
 }
