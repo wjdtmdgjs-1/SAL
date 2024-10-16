10f447c5d8aa2da2db87e7f08f279d7dbd7f7d30
Yeseo Park <ye_2002@naver.com>
2024-10-17 03:12:43 +0900

    Refector : 안쓰는 생성자 삭제

--- src/main/java/com/sparta/sal/domain/card/entity/Card.java
+++ src/main/java/com/sparta/sal/domain/card/entity/Card.java
@@ -40,12 +40,6 @@ public class Card extends Timestamped {
         this.attachment = filename;
     }
 
-    public Card(String title, String cardExplain, LocalDateTime deadline) {
-        this.cardTitle = title;
-        this.cardExplain = cardExplain;
-        this.deadline = deadline;
-    }
-
     public void modifyCard(ModifyCardRequest reqDto) {
         this.cardTitle = reqDto.getTitle();
         this.cardExplain = reqDto.getCardExplain();
